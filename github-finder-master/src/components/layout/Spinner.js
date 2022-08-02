591f4a9cf41d6ff296f71cf85a4d839d57a0823e
Arka Patra <arkapatra2009@gmail.com>
2022-08-02 23:46:43 +0530

    Delete github-finder-master directory

--- github-finder-master/src/components/layout/Spinner.js
+++
@@ -1,8 +0,0 @@
-import React, { Fragment } from 'react'
-import spinner from './spinner.gif';
-
-const Spinner = () => <Fragment>
-      <img src={spinner} alt="Loading..." style={{ width: '200px', margin: 'auto', display: 'block' }} />
-    </Fragment>
-
-export default Spinner
