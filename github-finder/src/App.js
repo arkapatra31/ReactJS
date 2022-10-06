8d54ded8e4d0510557abaa6a007bedcc2de4d19f
Arka <arkapatra2009@gmail.com>
2022-10-06 22:47:25 +0530

    GitHub Finder Navbar Issue

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
@@ -7,8 +8,8 @@ function App() {
       <div className="App">
         <div className="flex flex-col justify-between h-screen">
           <Navbar />
-
           <main>Content</main>
+          <Footer />
         </div>
       </div>
     </Router>
--- github-finder/src/components/layout/Navbar.jsx
+++ github-finder/src/components/layout/Navbar.jsx
@@ -11,6 +11,17 @@ function Navbar({ title }) {
             {title}
           </Link>
         </div>
+
+        <div className="flex-1 px-2 mx-2">
+          <div className="flex justify-end">
+            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+              Home
+            </Link>
+            <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
+              About
+            </Link>
+          </div>
+        </div>
       </div>
     </nav>
   );
