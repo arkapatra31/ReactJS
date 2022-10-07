a7c0156bf286abd90aea88c261ce4b9014beeb00
Arka <arkapatra2009@gmail.com>
2022-10-08 00:48:25 +0530

    Layout Pages

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -8,7 +11,14 @@ function App() {
       <div className="App">
         <div className="flex flex-col justify-between h-screen">
           <Navbar />
-          <main>Content</main>
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
           <Footer />
         </div>
       </div>
---
+++ github-finder/src/pages/About.jsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <div>
+        <h1 className="text-6xl mb-4">GitHub Finder App</h1>
+        <span className="text-1xl">
+            <p>This is a GitHub Finder project</p>
+            <p>Version : 1.0</p>
+        </span>
+    </div>
+  )
+}
+
+export default About
