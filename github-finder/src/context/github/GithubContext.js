82a70dfc22cf49577455358d16faef7c3086e394
Arka <arkapatra2009@gmail.com>
2022-10-11 00:45:57 +0530

    useReducer

---
+++ github-finder/src/context/github/GithubContext.js
@@ -0,0 +1,47 @@
+import { createContext, /**useState**/ useReducer } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext();
+
+const GITHUB_URL = process.env.REACT_APP__GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_FINDER_TOKEN;
+
+export const GithubProvider = ({ children }) => {
+  // const [users, setUsers] = useState([]);
+  // const [loading, setLoading] = useState(true);
+
+  /** Reducer Part Start */
+  const initialState = {
+    users : [],
+    loading : true
+  }
+
+  const[state, dispatch] = useReducer(githubReducer, initialState);
+
+  /** Reducer Part End */
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `${GITHUB_TOKEN}`,
+      },
+    });
+    const data = await response.json();
+
+    // setUsers(data);
+    // setLoading(false);
+
+    dispatch({
+      type: 'GET_USERS',
+      payload: data
+    })
+
+  };
+
+  return (
+    <GithubContext.Provider value={{ users: state.users, loading: state.loading, fetchUsers }}>
+      {children}
+    </GithubContext.Provider>
+  );
+};
+export default GithubContext;
