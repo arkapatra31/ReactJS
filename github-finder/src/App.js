147687301def22bef8561dfe85678a8056bfe1a8
Arka <arkapatra2009@gmail.com>
2022-10-10 23:20:48 +0530

    Using Context

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -4,25 +4,28 @@ import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import {GithubProvider} from "./context/github/GithubContext";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/notfound" element={<NotFound />} />
-              <Route path="/*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
+    <GithubProvider>
+      <Router>
+        <div className="App">
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className="container mx-auto px-3 pb-12">
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </GithubProvider>
   );
 }
 
--- github-finder/src/components/users/UserResults.jsx
+++ github-finder/src/components/users/UserResults.jsx
@@ -1,29 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useContext } from "react";
+import UserItem from "./UserItem";
 import Spinner from "../layout/Spinner";
+import GithubContext from "../../context/github/GithubContext";
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+
+  const {users, loading, fetchUsers} = useContext(GithubContext);
 
   useEffect(() => {
-    fetchRequest();
+    fetchUsers();
   }, []);
-
-  const fetchRequest = async () => {
-    const response = await fetch(`${process.env.REACT_APP__GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_FINDER_TOKEN}`,
-      },
-    });
-    const data = await response.json();
-    setUsers(data);
-    setLoading(false);
-  };
 
   if (!loading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => (
-          <h3 key={user.login}>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
