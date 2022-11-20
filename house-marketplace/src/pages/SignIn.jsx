bf65c82f53ac2e3ba76e1dc2e289062d9ffbc93d
Arka <arkapatra2009@gmail.com>
2022-11-20 23:37:25 +0530

    Google OAuth SignIn

--- house-marketplace/src/pages/SignIn.jsx
+++ house-marketplace/src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import OAuth from '../components/OAuth'
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -81,7 +82,7 @@ function SignIn() {
               </button>
             </div>
           </form>
-          {/* OAuth */}
+          <OAuth />
           <Link to="/sign-up" className="registerLink">
             Sign Up
           </Link>
--- house-marketplace/src/pages/SignUp.jsx
+++ house-marketplace/src/pages/SignUp.jsx
@@ -10,6 +10,7 @@ import { doc, setDoc, serverTimestamp } from "firebase/firestore";
 import db from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import OAuth from "../components/OAuth";
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
@@ -106,7 +107,7 @@ function SignUp() {
               </button>
             </div>
           </form>
-          {/* OAuth */}
+          <OAuth />
           <Link to="/sign-in" className="registerLink">
             Sign In
           </Link>
